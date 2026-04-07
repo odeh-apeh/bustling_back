30c8edac992d300a632030ddcff777376146fdd9
odeh-apeh <Gapeh10@gmail.com>
2026-04-07 15:28:28 +0100

    Update

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const router = express.Router();
-const { createTicket, getAllTicketsForUser, deleteTicket } = require("../controllers/ticketController");
+const { createTicket, getAllTicketsForUser, deleteTicket, updateTicketStatus } = require("../controllers/ticketController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
 router.post("/create", authMiddleware, createTicket);
