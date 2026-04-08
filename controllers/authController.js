5bd727e483ebff5798b86715dd47196ed4728175
odeh-apeh <Gapeh10@gmail.com>
2026-04-08 13:38:36 +0100

    Fixed id

--- controllers/authController.js
+++ controllers/authController.js
@@ -328,7 +328,7 @@ exports.createNewPassword = async (req, res) => {
             email: email,
             table: 'users',
             attribute:'email',
-            item:'email'
+            item:'otp'
         });
         if(!data){
             return res.status(400).json({
