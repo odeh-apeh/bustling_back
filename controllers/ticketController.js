137fd0fec488ed08b262204e1bc38294703147fb
odeh-apeh <Gapeh10@gmail.com>
2026-04-08 11:28:06 +0100

    Update

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -85,12 +85,17 @@ exports.emailUser = async (req, res) => {
   const { email, subject, message, ticket_id } = req.body;
 
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  connectionTimeout: 10000, // 10 sec
+  greetingTimeout: 10000,
+  socketTimeout: 10000
+});
 
   if (!email || !subject || !message) {
     return res.status(400).json({
